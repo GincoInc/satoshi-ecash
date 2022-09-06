ae356c5677116c556341d68a6108a9406ad9547e
GuiltyMorishita <morilliantblue@gmail.com>
2022-09-06 23:02:08 +0900

    :wrench: Fix: eCash

--- index.js
+++ index.js
@@ -1,70 +1,74 @@
 /**
- * @module satoshi-bitcoin
+ * @module satoshi-ecash
  */
 
-var Big = require('big.js');
+var Big = require('big.js')
 
 // @private
-var conversion = 100000000;
+var conversion = 100
 
 // es6 polyfill
 if (!Number.isInteger) {
-  Number.isInteger = function(num) {
-    return typeof num === 'number' && num % 1 === 0;
+  Number.isInteger = function (num) {
+    return typeof num === 'number' && num % 1 === 0
   }
 }
 
 // @private
 function toNumber(notNum) {
-  return Number(notNum);
+  return Number(notNum)
 }
 
 module.exports = {
-
   /**
-   * Convert Satoshi to Bitcoin
+   * Convert Satoshi to eCash
    * @param {number|string} satoshi Amount of Satoshi to convert. Must be a whole number
    * @throws {TypeError} Thrown if input is not a number or string
    * @throws {TypeError} Thrown if input is not a whole number or string format whole number
    * @returns {number}
    */
-  toBitcoin: function(satoshi) {
+  toECash: function (satoshi) {
     //validate arg
-    var satoshiType = typeof satoshi;
+    var satoshiType = typeof satoshi
     if (satoshiType === 'string') {
-      satoshi = toNumber(satoshi);
-      satoshiType = 'number';
+      satoshi = toNumber(satoshi)
+      satoshiType = 'number'
     }
-    if (satoshiType !== 'number'){
-      throw new TypeError('toBitcoin must be called on a number or string, got ' + satoshiType);
+    if (satoshiType !== 'number') {
+      throw new TypeError(
+        'toECash must be called on a number or string, got ' + satoshiType
+      )
     }
     if (!Number.isInteger(satoshi)) {
-      throw new TypeError('toBitcoin must be called on a whole number or string format whole number');
+      throw new TypeError(
+        'toECash must be called on a whole number or string format whole number'
+      )
     }
 
-    var bigSatoshi = new Big(satoshi);
-    return Number(bigSatoshi.div(conversion));
+    var bigSatoshi = new Big(satoshi)
+    return Number(bigSatoshi.div(conversion))
   },
 
   /**
-   * Convert Bitcoin to Satoshi
-   * @param {number|string} bitcoin Amount of Bitcoin to convert
+   * Convert eCash to Satoshi
+   * @param {number|string} eCash Amount of eCash to convert
    * @throws {TypeError} Thrown if input is not a number or string
    * @returns {number}
    */
-  toSatoshi: function(bitcoin) {
+  toSatoshi: function (eCash) {
     //validate arg
-    var bitcoinType = typeof bitcoin;
-    if (bitcoinType === 'string') {
-      bitcoin = toNumber(bitcoin);
-      bitcoinType = 'number';
+    var eCashType = typeof eCash
+    if (eCashType === 'string') {
+      eCash = toNumber(eCash)
+      eCashType = 'number'
     }
-    if (bitcoinType !== 'number'){
-      throw new TypeError('toSatoshi must be called on a number or string, got ' + bitcoinType);
+    if (eCashType !== 'number') {
+      throw new TypeError(
+        'toSatoshi must be called on a number or string, got ' + eCashType
+      )
     }
 
-    var bigBitcoin = new Big(bitcoin);
-    return Number(bigBitcoin.times(conversion));
-  }
-
-};
+    var bigECash = new Big(eCash)
+    return Number(bigECash.times(conversion))
+  },
+}
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,49 +1,57 @@
-var should = require('chai').should();
-var sb = require('../index.js');
+var should = require('chai').should()
+var sb = require('../index.js')
 
-describe('#toBitcoin', function() {
-  it('converts simple integer amounts', function() {
-    sb.toBitcoin(100000000).should.equal(1);
-    sb.toBitcoin(123456789012345).should.equal(1234567.89012345);
-  });
-  it('converts simple string amounts', function() {
-    sb.toBitcoin('100000000').should.equal(1);
-    sb.toBitcoin('123456789012345').should.equal(1234567.89012345);
-  });
+describe('#toECash', function () {
+  it('converts simple integer amounts', function () {
+    sb.toECash(100000000).should.equal(1000000)
+    sb.toECash(123456789012345).should.equal(1234567890123.45)
+  })
+  it('converts simple string amounts', function () {
+    sb.toECash('100000000').should.equal(1000000)
+    sb.toECash('123456789012345').should.equal(1234567890123.45)
+  })
 
-  it('converts to Bitcoin, not to Satoshi', function() {
-    sb.toBitcoin(98765).should.not.equal(9876500000000);
-  });
+  it('converts to eCash, not to Satoshi', function () {
+    sb.toECash(98765).should.not.equal(9876500)
+  })
 
-  it('converts and handles corner case rounding', function() {
-    sb.toBitcoin(46).should.equal(.00000046);
-  });
+  it('converts and handles corner case rounding', function () {
+    sb.toECash(46).should.equal(0.46)
+  })
 
-  it('handles TypeError input', function() {
-    sb.toBitcoin.bind(this, true).should.throw('toBitcoin must be called on a number or string');
-    sb.toBitcoin.bind(this, 1.1).should.throw('toBitcoin must be called on a whole number or string format whole number');
-  });
-});
+  it('handles TypeError input', function () {
+    sb.toECash
+      .bind(this, true)
+      .should.throw('toECash must be called on a number or string')
+    sb.toECash
+      .bind(this, 1.1)
+      .should.throw(
+        'toECash must be called on a whole number or string format whole number'
+      )
+  })
+})
 
-describe('#toSatoshi', function() {
-  it('converts simple integer amounts', function() {
-    sb.toSatoshi(0.00000001).should.equal(1);
-    sb.toSatoshi(98765).should.equal(9876500000000);
-  });
-  it('converts simple string amounts', function() {
-    sb.toSatoshi('0.00000001').should.equal(1);
-    sb.toSatoshi('98765').should.equal(9876500000000);
-  });
+describe('#toSatoshi', function () {
+  it('converts simple integer amounts', function () {
+    sb.toSatoshi(0.01).should.equal(1)
+    sb.toSatoshi(98765).should.equal(9876500)
+  })
+  it('converts simple string amounts', function () {
+    sb.toSatoshi('0.01').should.equal(1)
+    sb.toSatoshi('98765').should.equal(9876500)
+  })
 
-  it('converts to Satoshi, not to Bitcoin', function() {
-    sb.toSatoshi(123456789012345).should.not.equal(1234567.89012345);
-  });
+  it('converts to Satoshi, not to eCash', function () {
+    sb.toSatoshi(123456789012345).should.not.equal(1234567890123.45)
+  })
 
-  it('converts and handles corner case rounding', function() {
-    sb.toSatoshi(4.6).should.equal(460000000);
-  });
+  it('converts and handles corner case rounding', function () {
+    sb.toSatoshi(4.6).should.equal(460)
+  })
 
-  it('handles TypeError input', function() {
-    sb.toSatoshi.bind(this, true).should.throw('toSatoshi must be called on a number or string');
-  });
-});
+  it('handles TypeError input', function () {
+    sb.toSatoshi
+      .bind(this, true)
+      .should.throw('toSatoshi must be called on a number or string')
+  })
+})
